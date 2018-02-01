1714916426fab7d24af01c26accb4b5423480164
TheSamLloyd <samlloydj@gmail.com>
2018-01-31 19:43:20 -0500

    added more leaderboard functionality in js

--- assets/scripts/leaderboard.js
+++ assets/scripts/leaderboard.js
@@ -1,3 +1,23 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyBl7_O3pchKuUbj5TEBAcoOOpAlV-4RDRE",
+	authDomain: "bcs-whosaidit.firebaseapp.com",
+	databaseURL: "https://bcs-whosaidit.firebaseio.com",
+	projectId: "bcs-whosaidit",
+	storageBucket: "bcs-whosaidit.appspot.com",
+	messagingSenderId: "736508559692"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
 function leaderboardPull(){
-	
+	console.log(moment().startOf('day').format("YYMMDD"));
+	var leaderboardAllTime = firebase.database().ref('leaderboards/alltime');
+	leaderboardAllTime.on('value',function(snapshot){
+		console.log(snapshot.val())
+	})
+	var leaderboardAllTime = firebase.database().ref('leaderboards/');
+	leaderboardAllTime.on('value',function(snapshot){
+		console.log(snapshot.val())
+	})
 }
+leaderboardPull();
