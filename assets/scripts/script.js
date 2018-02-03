9d0b1928e99c1600cf555f100970ac4e121b9737
Thomas Hudson <snarfblam@gmail.com>
2018-02-03 11:35:04 -0500

    Created DbCommunicator class

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -60,7 +60,9 @@ function OAuthUtility() {
     this.provider = new firebase.auth.GoogleAuthProvider();
 }
 {
-    /** Returns a promise that resolves when authentication succeeds or fails */
+    /** Returns a promise that resolves when authentication succeeds or fails.
+     *  Will perform a redirect to a google sign-in page if the user is not logged in.
+     */
     OAuthUtility.prototype.authenticate = function authenticate() {
         var self = this;
 
@@ -89,6 +91,74 @@ function OAuthUtility() {
     };
 }
 
+/** Implements Client<->database and server<->database communication 
+ *  @constructor */
+function DbCommunicator(autoAuth) {
+    var config = {
+        apiKey: "AIzaSyBl7_O3pchKuUbj5TEBAcoOOpAlV-4RDRE",
+        authDomain: "bcs-whosaidit.firebaseapp.com",
+        databaseURL: "https://bcs-whosaidit.firebaseio.com",
+        projectId: "bcs-whosaidit",
+        storageBucket: "bcs-whosaidit.appspot.com",
+        messagingSenderId: "736508559692"
+    };
+    /** firebase.app.App object */ 
+    this.app = firebase.initializeApp(config);
+    this.database = firebase.database();
+
+    this.nodes = {
+        leaderboard: this.database.ref("leaderboard"),
+        players: this.database.ref("players"),
+        waitlist: this.database.ref("waitlist"),
+        chatMessages: this.database.ref("chatMessages"),
+        //currentRound: this.database.ref("currentRound"),
+        requests: this.database.ref("currentRound/requests"),
+        events: this.database.ref("currentRound/events"),
+        host: this.database.ref("host"),
+        ping: this.database.ref("ping"),
+        pong: this.database.ref("pong"),
+    };
+
+    this.auth = new OAuthUtility();
+    /** Resolves when the communitcator has authenticated */
+    this.authPromise = null;
+    if(autoAuth) {
+        this.authPromise = this.auth.authenticate();
+    }
+}
+
+/** Implements Client<->database and server<->database communication 
+ *  @constructor */
+function EventObject() {
+    /** The 'this' object in event handlers. */
+    this.handlerContext = null;
+
+    this.handlers = [];
+}
+{
+    /** Invokes all event handlers associated with the specified event */
+    EventObject.prototype.raise = function (eventName, args) {
+        var self = this;
+        handlers.forEach(function(handler) {
+            var handlerFunc = handler[eventName];
+            if(handlerFunc) {
+                handlerFunc.call(handlerContext || self, args);
+            }
+        });
+    };
+
+    /** Associates an event handler with this event source */
+    EventObject.prototype.on = function(handlerCollection_or_name, handler_if_name) {
+        // in the case of .on("event", handler), we turn it into on({event: handler});
+        if(typeof handlerCollection_or_name == "string") {
+            var newHandler = {};
+            newHandler[handlerCollection_or_name] = handler_if_name;
+        } 
+
+        this.handlers.push(handlerCollection_or_name);
+    };
+}
+
 // Test code
 $(document).ready(function () {
     // var tweeter = new TwitterReader();
@@ -100,21 +170,22 @@ $(document).ready(function () {
     //     });
     // Initialize Firebase
 
-    var config = {
-        apiKey: "AIzaSyBl7_O3pchKuUbj5TEBAcoOOpAlV-4RDRE",
-        authDomain: "bcs-whosaidit.firebaseapp.com",
-        databaseURL: "https://bcs-whosaidit.firebaseio.com",
-        projectId: "bcs-whosaidit",
-        storageBucket: "bcs-whosaidit.appspot.com",
-        messagingSenderId: "736508559692"
-    };
-    firebase.initializeApp(config);
-    var auth = new OAuthUtility();
-    auth.authenticate()
-        .then(function () {
-            $(document.body).append($("<img>").attr("src", "https://robohash.org/" + auth.user.displayName));
-        });
+    // var config = {
+    //     apiKey: "AIzaSyBl7_O3pchKuUbj5TEBAcoOOpAlV-4RDRE",
+    //     authDomain: "bcs-whosaidit.firebaseapp.com",
+    //     databaseURL: "https://bcs-whosaidit.firebaseio.com",
+    //     projectId: "bcs-whosaidit",
+    //     storageBucket: "bcs-whosaidit.appspot.com",
+    //     messagingSenderId: "736508559692"
+    // };
+    // firebase.initializeApp(config);
+    // var auth = new OAuthUtility();
+    // auth.authenticate()
+    //     .then(function () {
+    //         $(document.body).append($("<img>").attr("src", "https://robohash.org/" + auth.user.displayName));
+    //     });
 
+    var comm = new DbCommunicator(true);
 
     //document.write(JSON.stringify())
 });
